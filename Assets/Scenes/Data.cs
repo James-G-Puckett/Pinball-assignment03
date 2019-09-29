52e56d397b3c498c2b05c468771692f935c126ba
James-G-Puckett <54376462+James-G-Puckett@users.noreply.github.com>
2019-09-28 19:48:44 -0500

    Initial commit

---
+++ Assets/Scenes/Data.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Data : MonoBehaviour
+{
+    public static string PlayerName;
+    public static int score;
+    public InputField PlayerNameInput;
+    public Text showPlayerName;
+    public Text showMyScore;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        showPlayerName.text = PlayerName;
+    }
+
+    public void UpdateName() 
+    {
+        PlayerName = PlayerNameInput.text;
+        Debug.Log(PlayerName); 
+        showPlayerName.text = PlayerName;  
+    }
+    public void ShowName()
+    {
+        showPlayerName.text = PlayerName;
+        Debug.Log(PlayerName);
+    }
+
+    public void ShowScore()
+    {
+        showMyScore.text = score.ToString();
+    }
+}
---
+++ Assets/Scenes/Scenes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Scenes : MonoBehaviour
+{
+    public Dropdown MyDropDown;
+
+    public void Mutator()
+    {
+        switch (MyDropDown.value)
+        {
+            case 1:
+                SceneManager.LoadScene("Normal");
+                break;
+            case 2:
+                SceneManager.LoadScene("Big Flips");
+                break;
+            case 3:
+                SceneManager.LoadScene("Balls Everywhere");
+                break;
+            default:
+                //MyChoice.text = "Please Select";
+                break;
+        }
+    }
+    public void StartPlay()
+    {
+        SceneManager.LoadScene("Starter Scene");
+    }
+
+    public void StopPlaying()
+    {
+        SceneManager.LoadScene("Exit");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void ExitGame()
+    {
+        // exit the game for real play
+        //Application.Quit();
+        //does not work in Unity since it would  lose all settings. so:
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
